5a2403761f90ed7bc0fb2e785095919c74889872
nohabroha <noahysimon@gmail.com>
2018-06-15 15:44:48 -0500

    all things working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -15,7 +15,6 @@
     var spaces = [];
     for(var j = 0; j < compChoice.length; j++) {
         spaces.push("_ ");
-        document.write("_ ");
         console.log(spaces);
     }
 
@@ -26,7 +25,7 @@
     "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
     for(var i = 0; i < choices.length; i++) {
-        document.write(choices[i]);
+        document.getElementById("choices").innerHTML = "Choices: " + choices;
     }
 
     
@@ -41,30 +40,42 @@
     }
 
     var chances = 5;
-    var win = winnerWord.length;
     var points = 0; 
     var guessedLetters = [];
     document.getElementById("chances").innerHTML = "Chances: " + chances;
     document.getElementById("points").innerHTML = "Points: " + points;
     document.getElementById("output").innerHTML = "Your Progress " + spaces;
+    document.getElementById("guessedLetters").innerHTML = "Guessed Letters: " + guessedLetters;
 
     //listens for a key press
     document.addEventListener('keydown', function (event) {
 
         
-        //checks if it's in the winner word, if it is not a chance is lost
+        //checks if it's in the winner word
         console.log(winnerWord.includes(event.key))
+        
+        //checks if the letter has already been guessed.
+        for(var i = 0; i<guessedLetters.length; i++) {
+            if (event.key === guessedLetters[i]) {
+                alert("You already guessed this letter");
+                return;
+            }
+        }
+        
+        //checks if the user input is in the word or takes away a chance
         if(winnerWord.includes(event.key) === true) {
             console.log(event.key);
             guessedLetters.push(event.key);
-            console.log(guessedLetters);   
+            console.log(guessedLetters);
+            document.getElementById("guessedLetters").innerHTML = "Guessed Letters: " + guessedLetters;   
         } else {
             console.log(event.key);
             chances--;
             document.getElementById("chances").innerHTML = "Chances: " + chances;
             console.log(chances);
             guessedLetters.push(event.key);
-            console.log(guessedLetters); 
+            console.log(guessedLetters);
+            document.getElementById("guessedLetters").innerHTML = "Guessed Letters: " + guessedLetters;
         }
         
         //checks if the keypress is in the word then replaces it and gives you a point, if correct.
