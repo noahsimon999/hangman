5c38762cd8b6f32ae22da1d2d62571ba3f23dd28
Nohabroha <noahysimon@gmail.com>
2018-06-16 17:42:15 -0500

    working on the reset

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,9 +2,24 @@
 //hangman game
 //    -styling-
 //game mechanics
-    // create array of possible words
-    function hangman() {
-    
+    // create array of possible words    
+function reset() {
+    var randWord = Math.floor(Math.random()*words.length);
+    var spaces = [];
+    var choices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l",
+    "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+    var chances = 5;
+    var points = 0; 
+    var guessedLetters = [];
+
+    document.getElementById("choices").innerHTML = "Choices: " + choices;
+    document.getElementById("chances").innerHTML = "Chances: " + chances;
+    document.getElementById("output").innerHTML = "Your Progress " + spaces.join(" ");
+    document.getElementById("guessedLetters").innerHTML = "Guessed Letters: " + guessedLetters;
+    document.getElementById("winLose").innerHTML = " ";
+    document.getElementById("final").innerHTML = "Your Progress " + spaces;
+}
+
 
     var words = ["one", "two", "three", "four", "five", "six"];
     var win = 0;
@@ -13,7 +28,7 @@
     document.getElementById("win").innerHTML = "Win: " + win;
     document.getElementById("loss").innerHTML = "Loss: " + loss;
     // render blank spaces that can be filled in when guessed
-
+function blankSpaces() {
     var randWord = Math.floor(Math.random()*words.length);
     console.log(randWord);
     var compChoice = words[randWord];
@@ -23,8 +38,8 @@
         spaces.push("_ ");
         console.log(spaces);
     }
-
-    
+}
+    blankSpaces();
 
     // display all possible letters 
     var choices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l",
@@ -36,20 +51,20 @@
 
     
     // create winner array to check against
+    
+        var winnerWord = []
+        for(var i = 0; i < compChoice.length; i++) {
+            winnerWord.push(compChoice[i].charAt(0))
+            console.log(winnerWord);
+        }
 
-    var winnerWord = []
-    for(var i = 0; i < compChoice.length; i++) {
-        winnerWord.push(compChoice[i].charAt(0))
-        console.log(winnerWord);
-    }
-
-    var chances = 5;
-    var points = 0; 
-    var guessedLetters = [];
-    document.getElementById("chances").innerHTML = "Chances: " + chances;
-    document.getElementById("output").innerHTML = "Your Progress " + spaces;
-    document.getElementById("guessedLetters").innerHTML = "Guessed Letters: " + guessedLetters;
-
+        var chances = 5;
+        var points = 0; 
+        var guessedLetters = [];
+        document.getElementById("chances").innerHTML = "Chances: " + chances;
+        document.getElementById("output").innerHTML = "Your Progress " + spaces.join(" ");
+        document.getElementById("guessedLetters").innerHTML = "Guessed Letters: " + guessedLetters;
+    
     //listens for a key press
     document.addEventListener('keydown', function (event) {
 
@@ -57,6 +72,10 @@
         //checks if it's in the winner word
         console.log(winnerWord.includes(event.key))
         
+        if (chances === 0) {
+            reset();
+        }
+
         //checks if the letter has already been guessed.
         for(var i = 0; i<guessedLetters.length; i++) {
             if (event.key === guessedLetters[i]) {
@@ -86,7 +105,7 @@
             if(winnerWord[i] === event.key) {
                 spaces[i] = event.key;
                 console.log(spaces); 
-                document.getElementById("output").innerHTML = "Your Progress " + spaces;
+                document.getElementById("output").innerHTML = "Your Progress " + spaces.join(" ");
             }
             if (winnerWord[i] === event.key) {
                 console.log("you win +1");
@@ -96,22 +115,26 @@
     
         // checks win or loss conditions
         if (chances === 0) {
+            loss++;
+            document.getElementById("loss").innerHTML = "Loss: " + loss;
             console.log("you lose");
             document.getElementById("winLose").innerHTML = "Play Again!";
-            document.getElementById("final").innerHTML = "Your Progress " + spaces;
-            loss++;
+            document.getElementById("final").innerHTML = "Your Progress " + spaces.join(" ");
+            reset();
        
         } else if (points === winnerWord.length) {
+            win++;
+            document.getElementById("win").innerHTML = "Win: " + win;
             console.log("you win the game");
             document.getElementById("winLose").innerHTML = "~~~Winner!!!~~~";
-            document.getElementById("final").innerHTML = "Your Progress " + spaces;
-            win++;
+            document.getElementById("final").innerHTML = "Your Progress " + spaces.join(" ");
+            reset();
         }
         
         
     })
     
-}
+
     // give the hangman lives, and create a mechanism to take them away
     // create game over/ new game screen
 
