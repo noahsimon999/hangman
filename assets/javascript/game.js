8f1740cd5f4fba0687e0e57f025dc7492d43ca50
noahsimon999 <38762053+noahsimon999@users.noreply.github.com>
2018-06-17 17:40:25 -0500

    Add files via upload

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,44 +2,73 @@
 //hangman game
 //    -styling-
 //game mechanics
-    // create array of possible words    
-function reset() {
-    var randWord = Math.floor(Math.random()*words.length);
-    var spaces = [];
-    var choices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l",
-    "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-    var chances = 5;
-    var points = 0; 
-    var guessedLetters = [];
 
-    document.getElementById("choices").innerHTML = "Choices: " + choices;
-    document.getElementById("chances").innerHTML = "Chances: " + chances;
-    document.getElementById("output").innerHTML = "Your Progress " + spaces.join(" ");
-    document.getElementById("guessedLetters").innerHTML = "Guessed Letters: " + guessedLetters;
-    document.getElementById("winLose").innerHTML = " ";
-    document.getElementById("final").innerHTML = "Your Progress " + spaces;
-}
 
+
+
+// create array of possible words   
 
     var words = ["one", "two", "three", "four", "five", "six"];
     var win = 0;
     var loss = 0;
-    
+  
     document.getElementById("win").innerHTML = "Win: " + win;
     document.getElementById("loss").innerHTML = "Loss: " + loss;
+    
+    //reset
+function reset() {
     // render blank spaces that can be filled in when guessed
-function blankSpaces() {
+        var randWord = Math.floor(Math.random()*words.length);
+        var compChoice = words[randWord];
+        console.log(compChoice);
+        var spaces = [];
+        for(var j = 0; j < compChoice.length; j++) {
+            spaces.push("_ ");
+        }
+        console.log(spaces);
+    // display all possible letters 
+        var choices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l",
+        "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+    
+        for(var i = 0; i < choices.length; i++) {    
+            document.getElementById("choices").innerHTML = "Choices: " + choices;
+        }
+        // create winner array to check against
+        var winnerWord = []
+        for(var i = 0; i < compChoice.length; i++) {
+            winnerWord.push(compChoice[i].charAt(0))
+        }
+        console.log(winnerWord);
+        var chances = 5;
+        var points = 0; 
+        var guessedLetters = [];
+        document.getElementById("chances").innerHTML = "Chances: " + chances;
+        document.getElementById("output").innerHTML = "Your Progress " + spaces.join(" ");
+        document.getElementById("guessedLetters").innerHTML = "Guessed Letters: " + guessedLetters;
+        console.log(guessedLetters);
+        console.log(points);
+        console.log(chances);
+        return;
+
+        
+    }
+    
+    
+    
+    
+    
+    // render blank spaces that can be filled in when guessed
+
     var randWord = Math.floor(Math.random()*words.length);
-    console.log(randWord);
     var compChoice = words[randWord];
     console.log(compChoice);
     var spaces = [];
     for(var j = 0; j < compChoice.length; j++) {
         spaces.push("_ ");
-        console.log(spaces);
+        
     }
-}
-    blankSpaces();
+    console.log(spaces);
+    
 
     // display all possible letters 
     var choices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l",
@@ -51,38 +80,47 @@ function blankSpaces() {
 
     
     // create winner array to check against
-    
-        var winnerWord = []
-        for(var i = 0; i < compChoice.length; i++) {
-            winnerWord.push(compChoice[i].charAt(0))
-            console.log(winnerWord);
-        }
 
-        var chances = 5;
-        var points = 0; 
-        var guessedLetters = [];
-        document.getElementById("chances").innerHTML = "Chances: " + chances;
-        document.getElementById("output").innerHTML = "Your Progress " + spaces.join(" ");
-        document.getElementById("guessedLetters").innerHTML = "Guessed Letters: " + guessedLetters;
+    var winnerWord = []
+    for(var i = 0; i < compChoice.length; i++) {
+        winnerWord.push(compChoice[i].charAt(0))
+    }
+    console.log(winnerWord);
+
+    // console
+    var chances = 5;
+    var points = 0; 
+    var guessedLetters = [];
+    document.getElementById("chances").innerHTML = "Chances: " + chances;
+    document.getElementById("output").innerHTML = "Your Progress " + spaces.join(" ");
+    document.getElementById("guessedLetters").innerHTML = "Guessed Letters: " + guessedLetters;
+
     
+
+    
+
+
+
+
     //listens for a key press
     document.addEventListener('keydown', function (event) {
-
         
+   
+
+        console.log("points: " + points)
+        console.log(guessedLetters)
+        console.log(winnerWord)
         //checks if it's in the winner word
         console.log(winnerWord.includes(event.key))
-        
-        if (chances === 0) {
-            reset();
-        }
-
         //checks if the letter has already been guessed.
         for(var i = 0; i<guessedLetters.length; i++) {
+            
             if (event.key === guessedLetters[i]) {
                 alert("You already guessed this letter");
                 return;
             }
         }
+        console.log(guessedLetters);
         
         //checks if the user input is in the word or takes away a chance
         if(winnerWord.includes(event.key) === true) {
@@ -115,20 +153,19 @@ function blankSpaces() {
     
         // checks win or loss conditions
         if (chances === 0) {
-            loss++;
-            document.getElementById("loss").innerHTML = "Loss: " + loss;
             console.log("you lose");
             document.getElementById("winLose").innerHTML = "Play Again!";
-            document.getElementById("final").innerHTML = "Your Progress " + spaces.join(" ");
+            loss++;
+            document.getElementById("loss").innerHTML = "Loss: " + loss;
             reset();
        
         } else if (points === winnerWord.length) {
-            win++;
-            document.getElementById("win").innerHTML = "Win: " + win;
             console.log("you win the game");
             document.getElementById("winLose").innerHTML = "~~~Winner!!!~~~";
-            document.getElementById("final").innerHTML = "Your Progress " + spaces.join(" ");
+            win++;
+            document.getElementById("win").innerHTML = "Win: " + win;
             reset();
+            console.log(win);
         }
         
         
