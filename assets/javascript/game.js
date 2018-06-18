b3080e06d09b5b9eabd90e455d60a5bf7beca8dd
nohabroha <noahysimon@gmail.com>
2018-06-18 16:09:14 -0500

    with background styling

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -15,49 +15,8 @@
     document.getElementById("win").innerHTML = "Win: " + win;
     document.getElementById("loss").innerHTML = "Loss: " + loss;
     
-    //reset
-function reset() {
-    // render blank spaces that can be filled in when guessed
-        var randWord = Math.floor(Math.random()*words.length);
-        var compChoice = words[randWord];
-        console.log(compChoice);
-        var spaces = [];
-        for(var j = 0; j < compChoice.length; j++) {
-            spaces.push("_ ");
-        }
-        console.log(spaces);
-    // display all possible letters 
-        var choices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l",
-        "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-    
-        for(var i = 0; i < choices.length; i++) {    
-            document.getElementById("choices").innerHTML = "Choices: " + choices;
-        }
-        // create winner array to check against
-        var winnerWord = []
-        for(var i = 0; i < compChoice.length; i++) {
-            winnerWord.push(compChoice[i].charAt(0))
-        }
-        console.log(winnerWord);
-        var chances = 5;
-        var points = 0; 
-        var guessedLetters = [];
-        document.getElementById("chances").innerHTML = "Chances: " + chances;
-        document.getElementById("output").innerHTML = "Your Progress " + spaces.join(" ");
-        document.getElementById("guessedLetters").innerHTML = "Guessed Letters: " + guessedLetters;
-        console.log(guessedLetters);
-        console.log(points);
-        console.log(chances);
-        return;
 
-        
-    }
-    
-    
-    
-    
-    
-    // render blank spaces that can be filled in when guessed
+    // choose random word and render blank spaces that can be filled in when guessed
 
     var randWord = Math.floor(Math.random()*words.length);
     var compChoice = words[randWord];
@@ -68,7 +27,7 @@ function reset() {
         
     }
     console.log(spaces);
-    
+
 
     // display all possible letters 
     var choices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l",
@@ -87,26 +46,54 @@ function reset() {
     }
     console.log(winnerWord);
 
+
     // console
     var chances = 5;
     var points = 0; 
     var guessedLetters = [];
     document.getElementById("chances").innerHTML = "Chances: " + chances;
-    document.getElementById("output").innerHTML = "Your Progress " + spaces.join(" ");
+    document.getElementById("output").innerHTML = spaces.join(" ");
     document.getElementById("guessedLetters").innerHTML = "Guessed Letters: " + guessedLetters;
 
     
-
-    
-
-
+    //reset
+    function reset() {
+        // render blank spaces that can be filled in when guessed
+            var randWord = Math.floor(Math.random()*words.length);
+            var compChoice = words[randWord];
+            cofnsole.log(compChoice);
+            var spaces = [];
+            for(var j = 0; j < compChoice.length; j++) {
+                spaces.push("_ ");
+            }
+            console.log(spaces);
+        // display all possible letters 
+            var choices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l",
+            "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+        
+            for(var i = 0; i < choices.length; i++) {    
+                document.getElementById("choices").innerHTML = "Choices: " + choices;
+            }
+            // create winner array to check against
+            var winnerWord = [];
+            for(var i = 0; i < compChoice.length; i++) {
+                winnerWord.push(compChoice[i].charAt(0))
+            }
+            console.log(winnerWord);
+            var chances = 5;
+            var points = 0; 
+            var guessedLetters = [];
+            document.getElementById("chances").innerHTML = "Chances: " + chances;
+            document.getElementById("output").innerHTML = spaces.join(" ");
+            document.getElementById("guessedLetters").innerHTML = "Guessed Letters: " + guessedLetters;
+            console.log(guessedLetters);
+            console.log("reset points" + points);
+            console.log("reset chances" + chances);       
+        }
 
 
     //listens for a key press
     document.addEventListener('keydown', function (event) {
-        
-   
-
         console.log("points: " + points)
         console.log(guessedLetters)
         console.log(winnerWord)
@@ -122,6 +109,7 @@ function reset() {
         }
         console.log(guessedLetters);
         
+
         //checks if the user input is in the word or takes away a chance
         if(winnerWord.includes(event.key) === true) {
             console.log(event.key);
@@ -132,18 +120,19 @@ function reset() {
             console.log(event.key);
             chances--;
             document.getElementById("chances").innerHTML = "Chances: " + chances;
-            console.log(chances);
+            console.log("chances left: " + chances);
             guessedLetters.push(event.key);
             console.log(guessedLetters);
             document.getElementById("guessedLetters").innerHTML = "Guessed Letters: " + guessedLetters;
         }
         
+
         //checks if the keypress is in the word then replaces it and gives you a point, if correct.
         for(var i = 0; i < winnerWord.length; i++) {
             if(winnerWord[i] === event.key) {
                 spaces[i] = event.key;
                 console.log(spaces); 
-                document.getElementById("output").innerHTML = "Your Progress " + spaces.join(" ");
+                document.getElementById("output").innerHTML = spaces.join(" ");
             }
             if (winnerWord[i] === event.key) {
                 console.log("you win +1");
@@ -151,6 +140,7 @@ function reset() {
             }
         }
     
+
         // checks win or loss conditions
         if (chances === 0) {
             console.log("you lose");
@@ -165,7 +155,6 @@ function reset() {
             win++;
             document.getElementById("win").innerHTML = "Win: " + win;
             reset();
-            console.log(win);
         }
         
         
